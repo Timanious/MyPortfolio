1c83262204a4b43aa3a03e596af31df2ebcaa987
Timanious <timcoster@gmail.com>
2024-06-02 13:12:29 +0200

    Added SketchManager.js to illustrate better the usefulness of instancing

--- MyProjects/P5jsExamples/Scripts/Sketch2.js
+++ MyProjects/P5jsExamples/Scripts/Sketch2.js
@@ -1,75 +1,70 @@
 
-const sketch2 = function(p)
+const sketch2 = function (containerDivId) 
 {
-    const canvasContainer = document.getElementById("CanvasContainer2");
-    let shoeModel;
-
-    p.preload = function() 
+    return function (p) 
     {
-        shoeModel = p.loadModel('./3DModels/Shoe-3DModel/Shoe.obj', true);
-        shoeTexture = p.loadImage('./Textures/Shoe-Textures/Shoe-Albedo.jpg');
-    }      
+        const canvasContainer = document.getElementById(containerDivId);
+        let shoeModel;
 
-    p.setup = function()
-    {
-        // const canvasContainer = document.getElementById("CanvasContainer2");
-        p.describe("A rotating white cube in the center of a black canvas");
-        
-        // Create the P5js Canvas. Storing it in a variable isn't nessescary but could be useful later on.
-        let canvas = p.createCanvas(256,256, p.WEBGL);
-        
-        canvas.parent('CanvasContainer2');
+        p.preload = function () 
+        {
+            shoeModel = p.loadModel('./3DModels/Shoe-3DModel/Shoe.obj', true);
+            shoeTexture = p.loadImage('./Textures/Shoe-Textures/Shoe-Albedo.jpg');
+        }
 
-        // Resize the canvas to its parent's size.
-        p.resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
-    }
+        p.setup = function () 
+        {
+            // const canvasContainer = document.getElementById("CanvasContainer2");
+            p.describe("A rotating white cube in the center of a black canvas");
 
-    p.draw = function()
-    {
+            // Create the P5js Canvas. Storing it in a variable isn't nessescary but could be useful later on.
+            let canvas = p.createCanvas(256, 256, p.WEBGL);
 
-        let locX = p.mouseX - p.height / 2;
-        let locY = p.mouseY - p.width / 2;
+            canvas.parent(containerDivId);
 
-        // Set the canvas background color to the same color as the cards body background color to seriously blend in.
-        p.background(255, 150, 111);
-        
-        // Scale the coordinate system so that the size of the cube stays the same relative to the canvas size.
-        p.scale(canvasContainer.offsetWidth);
-    
-        // Push() begins a drawing group so that the translations within this group don't affect the objects after the group.
-        // See: https://p5js.org/reference/#/p5/push
-        p.push();
-        p.translate(0, 0, 0); // Position objects at the origin
-        p.scale(0.0045);      // Scale model down in size
-        // p.rotateZ((p.frameCount * 0.01) + 123); // Rotate the objects on the Z axis taking framerate into acount
-        // p.rotateX((p.frameCount * 0.01) + 123);
-        p.angleMode(p.DEGREES);
-        p.rotateX(180);
-        p.rotateY((-p.frameCount * 1));
-        p.ambientLight(200);
-        p.directionalLight(255,255,255,0.5,0.75,0);
-        p.pointLight(255, 255, 255, locX, locY, 100);
-        p.specularMaterial(100);
-        // p.ambientMaterial(50);
-        p.shininess(10);
-        p.noStroke();
-        p.texture(shoeTexture);
-        p.model(shoeModel);
-        p.pop(); // End this drawing group. 
-    }
+            // Resize the canvas to its parent's size.
+            p.resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
+        }
 
-    p.windowResized = function()
-    {
-        p.resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
+        p.draw = function () 
+        {
+
+            // let locX = p.mouseX - p.height / 2;
+            // let locY = p.mouseY - p.width / 2;
+
+            // Set the canvas background color to the same color as the cards body background color to seriously blend in.
+            p.background(255, 150, 111);
+
+            // Scale the coordinate system so that the size of the cube stays the same relative to the canvas size.
+            p.scale(canvasContainer.offsetWidth);
+
+            // Push() begins a drawing group so that the translations within this group don't affect the objects after the group.
+            // See: https://p5js.org/reference/#/p5/push
+            p.push();
+
+                p.translate(0, 0, 0); // Position objects at the origin
+                p.scale(0.0045);      // Scale model down in size
+                // p.rotateZ((p.frameCount * 0.01) + 123); // Rotate the objects on the Z axis taking framerate into acount
+                // p.rotateX((p.frameCount * 0.01) + 123);
+                p.angleMode(p.DEGREES);
+                p.rotateX(180);
+                p.ambientLight(200);
+                p.rotateY((-p.frameCount * 1));
+                // p.pointLight(255, 255, 255, locX, locY, 100);
+                p.specularMaterial(100);
+                // p.ambientMaterial(50);
+                p.shininess(10);
+                p.noStroke();
+                p.texture(shoeTexture);
+                p.model(shoeModel);
+                p.directionalLight(255, 255, 255, 0.5, 0.75, 0);
+            
+            p.pop(); // End this drawing group. 
+        }
+
+        p.windowResized = function () 
+        {
+            p.resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
+        }
     }
 }
-
-let myp52 = new p5(sketch2);
-
-
-// // Resize the canvas to its parent's size when the window is resized.
-// function windowResized() 
-// {
-//     const canvasContainer = document.getElementById("CanvasContainer2");
-//     resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
-// }
---
+++ MyProjects/P5jsExamples/Scripts/SketchManager.js
@@ -0,0 +1,6 @@
+// Create a new instance in memory of the P5js 'sketch', 
+// passing in the conainer div id that the P5js canvas 
+// needs to be parented to as an argument.
+new p5(sketch1('CanvasContainer1'));
+
+new p5(sketch2('CanvasContainer2'));
