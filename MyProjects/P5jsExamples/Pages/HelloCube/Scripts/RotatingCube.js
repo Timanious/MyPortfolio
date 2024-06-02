0d49cf3c383221bb64c150cfee893ca847e68fe5
Timanious <timcoster@gmail.com>
2024-06-02 10:00:30 +0200

    Working on some P5js Examples.

--- MyProjects/P5jsExamples/Pages/HelloCube/Scripts/RotatingCube.js
+++ MyProjects/P5jsExamples/Pages/HelloCube/Scripts/RotatingCube.js
@@ -1,40 +1,48 @@
 
+const canvasContainer = document.getElementById("CanvasContainer");
+
 function setup()
 {
-    // const canvasContainer = document.getElementById("CanvasContainer");
+    // An 'alt' discription of what is visible on the canvas element for visually impared people using a screen reader. 
     describe("A rotating white cube in the center of a black canvas");
     
     // Create the P5js Canvas. Storing it in a variable isn't nessescary but could be useful later on.
     let canvas = createCanvas(256,256, WEBGL);
-    
+
+    // Resize the canvas to its parent's size.
+    resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
+
+    // Parent the canvas to the CanvasContainer div, meaning that the canvas will become a child of the CanvasContainer.
+    // About 'parenting' objects, think of it like this: 
+    // Children may move about freely within their parents space but when their parents move the children move with them.)
     canvas.parent('CanvasContainer');
 }
 
 function draw()
-{ 
-    const canvasContainer = document.getElementById("CanvasContainer");
-    // Scale the coordinate system so that the size of the cube stays the same relative to the canvas size.
-    scale(canvasContainer.offsetWidth);
+{   
+    // Set the canvas background color to black.  
     background(0);
+    
+    // Scale the x,y,z coordinate system by the size of its container so that the size of the cube stays the same relative to the canvas size.
+    scale(canvasContainer.offsetWidth);
     
     // Display the canvas' width.
     //text(width, 42, 54);
 
-    // Push() begins a drawing group so that the translations within this group don't affect the objects after the group.
+    // Push() begins a drawing group so that the translations within this group don't affect the objects translated after the group.
     // See: https://p5js.org/reference/#/p5/push
     push();
-    translate(0, 0, 0); // Position objects at the origin
-    rotateZ(frameCount * 0.01); // Rotate the objects on the Z axis taking framerate into acount
+    translate(0, 0, 0);             // Translate/Position objects at the origin/center of the 3D world.
+    rotateZ(frameCount * 0.01);     // Rotate the objects on the Z-axis taking framerate into acount
     rotateX(frameCount * 0.01);
     rotateY(frameCount * 0.01);
-    box(0.5, 0.5, 0.5); // Draw a cube
-    pop(); // End this drawing group. 
+    box(0.5, 0.5, 0.5);             // Draw a cube
+    pop();                          // End this drawing group. 
 }
 
-// Resize the canvas to its parent's size when the window is resized.
 function windowResized() 
 {
-    const canvasContainer = document.getElementById("CanvasContainer");
+    // Resize the canvas to its parent's size when the window is resized.
     resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
 }
 
---
+++ MyProjects/P5jsExamples/Scripts/Sketch1.js
@@ -0,0 +1,47 @@
+const sketch1 = function(p)
+{
+    const canvasContainer = document.getElementById('CanvasContainer1');
+
+    p.setup = function()
+    {
+        p.describe("A rotating white cube in the center of a black canvas");
+        
+        // Create the P5js Canvas. Storing it in a variable isn't nessescary but could be useful later on.
+        let canvas = p.createCanvas(256,256, p.WEBGL);
+        
+        // Parent the canvas to the CanvasContainer div.
+        canvas.parent('CanvasContainer1');
+
+        // Resize the canvas to its parent's size.
+        p.resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
+    }
+
+    p.draw = function()
+    {
+        // Set the canvas background color to the same color as the cards body background color to seriously blend in.
+        p.background(255, 150, 111);
+
+        // Scale the coordinate system so that the size of the cube stays the same relative to the canvas size.
+        p.scale(canvasContainer.offsetWidth);
+        
+        // Display the canvas' width.
+        //text(width, 42, 54);
+    
+        // Push() begins a drawing group so that the translations within this group don't affect the objects after the group.
+        // See: https://p5js.org/reference/#/p5/push
+        p.push();
+        p.translate(0, 0, 0); // Position objects at the origin
+        p.rotateZ(p.frameCount * 0.01); // Rotate the objects on the Z axis taking framerate into acount
+        p.rotateX(p.frameCount * 0.01);
+        p.rotateY(p.frameCount * 0.01);
+        p.box(0.5, 0.5, 0.5); // Draw a cube
+        p.pop(); // End this drawing group. 
+    }
+
+    p.windowResized = function()
+    {
+        p.resizeCanvas(canvasContainer.offsetWidth, canvasContainer.offsetWidth);
+    }
+}
+
+let myp5 = new p5(sketch1);
