5e5773f14d7412cbd6a4736a606eec566fadbded
Timanious <timcoster@gmail.com>
2024-06-11 07:14:58 +0200

    New Tutorial Project added PostDataToServer

---
+++ MyProjects/PostDataToServer/Public/Scripts/Client.js
@@ -0,0 +1,58 @@
+// Step 14 -
+// Make up an object called something like data with the information that we want to send to the server.
+const data = 
+{
+    message: 'Hello from client!',
+}
+
+// Step 15 -
+// If this were a fetch get request then we would simply do a fetch to the /api endpoint like this:
+// fetch('/api');
+
+// But what we want to do is send a POST so we need to send a second argument which is just a
+// JavaScript object. Call it something like options and set it up seperately as a variable so we can examin
+// it more closelier..
+// See: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+
+const options = 
+{
+    // The first property that needs to be put into the options block is the method that we're using
+    // and that method is a POST.
+    method: 'POST',
+
+    // There is a lot of informatiod that can be put into the options. If you want to look at all the posibilities
+    // like the 'mode', 'cache' and 'credentials' then go to the link, above but the key thing that we want is the 'body'
+    // of the POST request. The body is where we package up all of our data that we want to post.
+    // Even though there are different ways to send the data what we want to do is send it as a text string so
+    // we want to take the JavaScript object data and make it into a JSON string.
+    body: JSON.stringify(data),
+
+    // There is one other piece that's important.. We're specifically sending data in JSON format so it is useful to
+    // specify that in what is called a header. The headers are where we specify the type of data
+    // that we're sending. In this case we're sending JSON data so we need to specify that.
+    // A header in this context is something that can be packaged along with any POST or GET request that is moving between
+    // a client and the server, and it's a way of adding some additional meta information. More information is in the Mozilla docs.
+    // Headers have a name a colon and a value. 
+    // (Usually the headers get put before the body property but for the sake of the tutorial steps order it is put below here)
+    headers: 
+    {
+        "Content-Type": "application/json",
+    },
+
+    // Even though there is more that we can put in the options block the basic pieces that we need are:
+    // 1. A request to sent data as JSON,
+    // 2. A header telling that the data we're sending is going to be JSON,
+    // 3. A fetch call telling that we want to post it to the /api endpoint.
+}
+
+fetch('/api', options);
+
+// Step 16 -
+// So now we get the message data, we put it into a JavaScript object, package it as a POST and send it tou our /api endpoint 
+// and in the server we receive it and console log it.
+// Restart the server with command $ node Server.js and refresh the page in the browser. To see the console.log messages 
+// look in the VSCode terminal and you will see a message telling us that we've received a POST request but the data from
+// the body is still undefined. This is because we still need to add the ability to parse incoming data as JSON to our server.
+// The way to do that is with the express function express.json(), similarly to how we've used express.static() to tell the server
+// that we want to host static files. So go back to Server.js and add webServerApp.use(express.json());,
+// below the webServerApp.use(epress.static()) line.
---
+++ MyProjects/PostDataToServer/Server.js
@@ -0,0 +1,74 @@
+// Server.js
+console.log('Hello from Server!');
+
+// Setting up an Express webserver using Node.js.
+// Follows the tutorial of The Coding Train "Working with Data and APIs in JavaScript - 2.1 Server-side with Node.js":
+// https://www.youtube.com/watch?v=wxbQP1LMZsw&t=356s
+// See the official Express server documentation over here: 
+// https://expressjs.com/
+
+// Step 8 -
+// In order to have access to the Express package we need to use require() 
+// and we can at the same time store a reference to express in a constant.
+// This is basically like an import statement.
+// The reason we want to do that is because we want to create a web application.
+import express from 'express';
+
+// Step 9 -
+// The way that we can create the web app is by calling the express() function
+// The whole Node Express package basically comes in as a function that we can execute and put in a constant.
+const webServerApp = express();
+const port         = 3000;
+
+// Step 10 -
+// Start listening at a port number and subscribe a function to the callback event for when the server starts listening.
+webServerApp.listen(port, OnServerStartListening());
+
+function OnServerStartListening()
+{
+    console.log(`Webserver listening at port ${port}`);
+}
+
+// Step 11 -
+// Start the server in the terminal with command $ node Server.js.
+// The server is now listening for requests so when you go to localhost:3000 in your webbrowser you should see 'Cannot GET /'.
+// This is because the server isn't actually serving up the index.html file just yet.
+
+// Step 12 -
+// To have the Server serve up the index.html file, create a folder named Public and put your index.html file into it.
+// Then add the following code to your Server.js file to tell the server to serve up the files in the Public folder.
+webServerApp.use(express.static('Public'));
+
+// Step 17 - Add the ability to parse incoming data as JSON to our server.
+// The way to do that is with the express function express.json(): 
+// webServerApp.use(express.json());
+
+// Step 18 -
+// We can also put some options into the function's arguments to control or limit what is possible in terms of receiving data,
+// all of that is documented on the Express website over here: https://expressjs.com/en/api.html#express.json
+// As an example let's add the limit property to specify the maximum size of a body that is coming in to be 1mb as a starting point:
+webServerApp.use(express.json({ limit: '1mb' }));
+
+// Step 19 -
+// Restart the server with terminal command $ node Server.js and refresh the page in the browser. If you look in the terminal in VSCode
+// you will now see the message from the POST's body printed out correctly!
+
+// Step 13 -
+// Setting up Post request Routes
+// See: https://expressjs.com/en/guide/routing.html#route-methods
+// Following the steps of Working with Data and APIs in JavaScript - 2.3 HTTP Post Request with fetch():
+// https://www.youtube.com/watch?v=Kw5tC5nQMRY&t=0s
+
+// The post method takes two arguments. The first is the adress for where we want to receive that post and the second is
+// a callback function for looking at the information coming in and sending a response back. 
+webServerApp.post('/api', (request, response) => OnPostRequestCallback(request, response))
+
+function OnPostRequestCallback(request, response)
+{
+    console.log('Received a POST request: ' + request);
+    console.log('Request body: ' + request.body);
+    console.log('Body message property value: ' + request.body.message);
+}
+
+// The next step is to have the client send something to this particular endpoint with a post so we can now move over to
+// the client code which is in ./Public/Scripts/Client.js so see you there.
